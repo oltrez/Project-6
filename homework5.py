3eae192d3b9509bf5788edeab5c4ee9f95fd7250
oltrez <166997944+oltrez@users.noreply.github.com>
2024-05-11 22:16:49 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['персик', 'манго', 'ананас', 'киви', 'яблоко']
+print(my_list)
+print(my_list[0:5:4])
+print(my_list[2:5])
+my_list[2]= 'клубника'
+print(my_list)
+
+my_dict = {'dog': 'собака', 'cat': 'кошка', 'duck': 'утка'}
+print(my_dict)
+print(my_dict['dog'])
+my_dict['bull'] = ('бык')
+print(my_dict)
